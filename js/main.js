ecd15e66b371752c833b8601f75e8233566a7998
Yan Alexandre <61830805+YanAlexandrebz@users.noreply.github.com>
2021-02-02 14:02:52 -0300

    Adicionando arquivo main com extensão js

---
+++ js/main.js
@@ -0,0 +1,65 @@
+/*
+var nome = "Yan Alexandre";
+var idade = 21;
+var idade2 = 30;
+var frase = "Yan Alexandre";
+
+//alert(" Nome: " + nome + " Idade: " + idade);
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.replace("Yan","Yago").toUpperCase());
+*/
+
+/*
+var lista = ["maca","pera","laranja"];
+lista.push("uva"); //adiciona um item na lista
+lista.pop(); //remove o último elemento da lista
+
+console.log(lista[2]);
+console.log(lista.reverse()); //traz os elementos em ordem invertida
+console.log(lista.toString()); //transforma os elementos em String
+console.log(lista.join(" - ")); //separa os itens da lista quando impresso na tela
+*/
+
+/*
+var frutas =[{nome:"Pera", cor:"Verde"},{nome:"Maca",cor:"Vermelho"}];
+console.log(frutas);
+*/
+
+//CONDICIONAIS
+
+/*
+var idade = prompt("Qual sua idade");
+if(idade >= 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}
+*/
+
+//LAÇOS  
+//WHILE
+/*
+var count = 5;
+while(count <= 5){
+    console.log(count);
+    count++;
+}
+*/
+
+//FOR
+/*
+for(var count = 0; count <=5; count++){
+    alert(count);
+}
+*/
+
+//var d = new Date();
+//alert(d.getDate());
+//alert(d.getMonth()+1);
+//alert(d.getMinutes());
+//alert(d.getHours());
+
+
